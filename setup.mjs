aab1f1c8bc602da8e710d559d961e03aa45da4d3
Benjamin Barreto <benjamin.barreto@shopify.com>
2022-11-04 17:17:56 +0100

    Use JS to install oh-my-zsh and powerlevel10k

--- setup.mjs
+++ setup.mjs
@@ -1,5 +1,13 @@
 #!/usr/bin/env zx
 
+const errorText = chalk.bold.red;
+const warningText = chalk.bold.yellow;
+
+const reportError = (message, error = new Error()) => {
+  console.log(errorText(message))
+  console.error(error)
+}
+
 const symlink = (file, target) => {
   // Check if file and target exist
   // Only create symlink if file and target both exist.
@@ -12,16 +20,48 @@ const backup = (file) => {
   // Should we fail gracefully or exit the script with an error?
 }
 
+const installOhmyzsh = async () => {
+  try {
+    const exists = await fs.pathExists(path.join(os.homedir(), '.oh-my-zsh'))
+
+    if (exists) {
+      console.log(warningText('Skipping oh-my-zsh, already installed!'))
+    } else {
+      await $`sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+    }
+  } catch (error) {
+    reportError('Problem installing oh-my-zsh', error)
+    exit(1)
+  }
+}
+
+const installP10K = async () => {
+  try {
+    const p10kPath = path.join(
+      os.homedir(),
+      '.oh-my-zsh',
+      'custom',
+      'themes',
+      'powerlevel10k'
+    )
+    const exists = await fs.pathExists(p10kPath)
+
+    if (exists) {
+      console.log(warningText('Skipping powerlevel10k, already installed!'))
+    } else {
+      await $`git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${p10kPath}`
+    }
+  } catch (error) {
+    reportError('Problem installing powerlevel10k', error)
+    exit(1)
+  }
+}
+
 const runTasks = async () => {
   const isSpin = !!process.env.SPIN
 
-  try {
-    console.log(`isSpin: ${isSpin}`)
-    process.exit(0)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
+  await installOhmyzsh()
+  await installP10K()
 }
 
 runTasks()
