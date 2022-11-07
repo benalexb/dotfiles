24108f1e7ea24e6b638e6b4c99ddf5fd5cbdb751
Benjamin Barreto <benjamin.barreto@shopify.com>
2022-11-07 10:45:05 +0100

    Added logic to install zsh plugins

--- setup.mjs
+++ setup.mjs
@@ -1,7 +1,8 @@
 #!/usr/bin/env zx
 
-const errorText = chalk.bold.red;
-const warningText = chalk.bold.yellow;
+const errorText = chalk.bold.red
+const warningText = chalk.bold.yellow
+const infoText = chalk.bold.white
 
 const reportError = (message, error = new Error()) => {
   console.log(errorText(message))
@@ -27,6 +28,7 @@ const installOhmyzsh = async () => {
     if (exists) {
       console.log(warningText('Skipping oh-my-zsh, already installed!'))
     } else {
+      console.log(infoText('-> Installing oh-my-zsh'))
       await $`sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
     }
   } catch (error) {
@@ -37,22 +39,50 @@ const installOhmyzsh = async () => {
 
 const installP10K = async () => {
   try {
-    const p10kPath = path.join(
-      os.homedir(),
-      '.oh-my-zsh',
-      'custom',
-      'themes',
-      'powerlevel10k'
-    )
+    const p10kPath = path.join(os.homedir(), '.oh-my-zsh', 'custom', 'themes', 'powerlevel10k')
     const exists = await fs.pathExists(p10kPath)
 
     if (exists) {
       console.log(warningText('Skipping powerlevel10k, already installed!'))
     } else {
+      console.log(infoText('-> Installing powerlevel10k'))
       await $`git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${p10kPath}`
     }
   } catch (error) {
     reportError('Problem installing powerlevel10k', error)
+    process.exit(1)
+  }
+}
+
+const installPlugin = async (pluginName, pluginDir, pluginRepo) => {
+  const pluginExists = await fs.pathExists(pluginDir)
+  if (pluginExists) {
+    console.log(warningText(`Skipping ${pluginName}, already installed!`))
+  } else {
+    console.log(infoText(`-> Installing ${pluginName}`))
+    $`git clone ${pluginRepo} ${pluginDir}`
+  }
+}
+
+const installZSHPlugins = async () => {
+  try {
+    const pluginsDir = path.join(os.homedir(), '.oh-my-zsh', 'custom', 'plugins')
+    await fs.ensureDir(pluginsDir)
+
+    installPlugin(
+      'zsh-syntax-highlighting',
+      path.join(pluginsDir, 'zsh-syntax-highlighting'),
+      'https://github.com/zsh-users/zsh-syntax-highlighting'
+    )
+
+    installPlugin(
+      'zsh-autosuggestions',
+      path.join(pluginsDir, 'zsh-autosuggestions'),
+      'https://github.com/zsh-users/zsh-autosuggestions'
+    )
+
+  } catch(error) {
+    reportError('Problem installing zsh plugins')
     process.exit(1)
   }
 }
@@ -62,6 +92,7 @@ const runTasks = async () => {
 
   await installOhmyzsh()
   await installP10K()
+  await installZSHPlugins()
 }
 
 runTasks()
