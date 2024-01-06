e7d60d0a0b23bbf4e7991c6bbbd6bdbe67e85bc9
Benjamin Barreto <benalexb@gmail.com>
2024-01-06 23:57:31 +0100

    Added debian install script

---
+++ config/debian/.zshrc
@@ -0,0 +1,20 @@
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+plugins=(gitfast git last-working-dir common-aliases sublime history-substring-search zsh-autosuggestions zsh-syntax-highlighting)
+source $ZSH/oh-my-zsh.sh
+
+# Store your own aliases in the ~/.aliases file and load it
+[[ -f "$HOME/.aliases" ]] && source "$HOME/.aliases"
+
+# Set editors
+export BUNDLER_EDITOR=code
+export EDITOR="vim"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
---
+++ install-debian.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Create symlinks and handle existing files or symlinks
+create_symlink() {
+  local source=$1
+  local target=$2
+
+  if [ -L "$target" ]; then
+    rm "$target"
+  elif [ -e "$target" ]; then
+    mv "$target" "${target}_bkup"
+  fi
+
+  ln -s "$source" "$target"
+}
+
+# Remove existing directory before git clone
+clone_and_replace() {
+  local repo=$1
+  local target_dir=$2
+
+  if [ -d "$target_dir" ]; then
+    rm -rf "$target_dir"
+  fi
+
+  git clone "$repo" "$target_dir"
+}
+
+# Change to the home directory
+cd $HOME
+
+# 1. Install ohmyzsh only if $ZSH is not set
+if [ -z "$ZSH" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# 2. Install powerlevel10k
+clone_and_replace "https://github.com/romkatv/powerlevel10k.git" "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+
+# 3. Install zsh-syntax-highlighting
+clone_and_replace "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
+# 4. Install zsh-autosuggestions
+clone_and_replace "https://github.com/zsh-users/zsh-autosuggestions" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+
+# 5. Create symlinks for .aliases and .p10k.zsh
+create_symlink "$HOME/dotfiles/config/common/.aliases" "$HOME/.aliases"
+create_symlink "$HOME/dotfiles/config/common/.p10k.zsh" "$HOME/.p10k.zsh"
+
+# 6. Create symlink for .zshrc
+create_symlink "$HOME/dotfiles/config/debian/.zshrc" "$HOME/.zshrc"
+
+# 7. Set up git configs
+git config --global user.name "Benjamin Barreto"
+git config --global user.email "benalexb@gmail.com"
+git config --global core.editor "code -w"
+git config --global core.pager "delta"
+git config --global init.defaultbranch "master"
+git config --global interactive.difffilter "delta --color-only --features=interactive"
+git config --global --add include.path "${HOME}/dotfiles/config/common/delta.gitconfig"
+git config --global --add include.path "${HOME}/dotfiles/config/common/delta-themes.gitconfig"
