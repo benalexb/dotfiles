b44a36c7b86890cb902f987961050b43514cf4ad
Ben Alex Barreto <benalexb@gmail.com>
2024-08-27 23:09:24 +0200

    Update install-popos.sh

--- install-popos.sh
+++ install-popos.sh
@@ -53,6 +53,7 @@ install_fonts() {
     # Loop through each font file and copy it
     for font in "$FONTS_SOURCE_DIR"/*/*.{ttf,otf}; do
         if [ -f "$font" ]; then
+            log INFO "Processing font: $font"
             local target_font="$LOCAL_FONT_DIR/$(basename "$font")"
 
             if [ ! -e "$target_font" ]; then
@@ -65,6 +66,8 @@ install_fonts() {
             else
                 log INFO "Skipping font: $(basename "$font") (already installed)"
             fi
+        else
+            log WARNING "No valid font files found at: $font"
         fi
     done
 
@@ -88,71 +91,4 @@ install_zsh() {
             exit 1
         }
     else
-        log INFO "zsh is already installed."
-    fi
-}
-
-# Function to install oh-my-zsh if not present
-install_oh_my_zsh() {
-    if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        log INFO "Installing Oh My Zsh..."
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
-            log ERROR "Failed to install Oh My Zsh."
-            exit 1
-        }
-    else
-        log INFO "Oh My Zsh is already installed."
-    fi
-}
-
-# Function to install zsh plugins and themes
-install_zsh_plugins_and_themes() {
-    log INFO "Installing zsh plugins and themes"
-    clone_or_update_repo "https://github.com/romkatv/powerlevel10k.git" "$ZSH_CUSTOM/themes/powerlevel10k"
-    clone_or_update_repo "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-    clone_or_update_repo "https://github.com/zsh-users/zsh-autosuggestions.git" "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-}
-
-# Function to set up git configuration
-setup_git_config() {
-    log INFO "Setting up Git configuration"
-    git config --global user.name "Benjamin Barreto"
-    git config --global user.email "benalexb@gmail.com"
-    git config --global core.editor "vim"
-    git config --global core.pager "delta"
-    git config --global init.defaultbranch "master"
-    git config --global interactive.difffilter "delta --color-only --features=interactive"
-    git config --global --add include.path "${HOME}/dotfiles/config/common/delta.gitconfig"
-    git config --global --add include.path "${HOME}/dotfiles/config/common/delta-themes.gitconfig"
-}
-
-# Main function to coordinate the setup
-main() {
-    log INFO "Starting setup script"
-
-    install_zsh
-    install_oh_my_zsh
-    install_fonts
-    install_zsh_plugins_and_themes
-
-    create_symlink "$HOME/dotfiles/config/common/.aliases" "$HOME/.aliases"
-    create_symlink "$HOME/dotfiles/config/common/.p10k.zsh" "$HOME/.p10k.zsh"
-    create_symlink "$HOME/dotfiles/config/common/.vimrc" "$HOME/.vimrc"
-    create_symlink "$HOME/dotfiles/config/debian/.zshrc" "$HOME/.zshrc"
-
-    setup_git_config
-
-    if [ -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
-        log INFO "Sourcing Oh My Zsh..."
-        source "$HOME/.oh-my-zsh/oh-my-zsh.sh"
-    else
-        log ERROR "Oh My Zsh installation seems to have failed."
-        exit 1
-    fi
-
-    log INFO "Setup completed. Starting zsh..."
-    exec zsh
-}
-
-# Run the main function
-main
+       
